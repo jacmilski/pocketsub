c6d6f6601bf34970714e7aab0290d892e9b897ba
Jacek Michalski <jacmilski@gmail.com>
2024-02-09 16:33:31 +0100

    Prettier formating

--- app/(application)/(app)/dashboard/page.tsx
+++ app/(application)/(app)/dashboard/page.tsx
@@ -1,7 +1,5 @@
 export default function DashboardPage() {
   return (
-    <main className="mx-4  px-0 text-center text-zinc-900">
-      Dashboard
-    </main>
+    <main className="mx-4  px-0 text-center text-zinc-900">Dashboard</main>
   );
 }
--- app/(application)/(app)/subscriptions/page.tsx
+++ app/(application)/(app)/subscriptions/page.tsx
@@ -1,7 +1,5 @@
 export default function SubscriptionsPage() {
   return (
-    <main className="mx-4  px-0 text-center text-zinc-900">
-      Subscriptions
-    </main>
+    <main className="mx-4  px-0 text-center text-zinc-900">Subscriptions</main>
   );
 }
