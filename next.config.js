6106d88fa2d1c6b94a87ebc2e5d6acaec58666f7
Jacek Michalski <jacmilski@gmail.com>
2024-02-09 13:17:23 +0100

    Disabled clerk-middleware

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    pageExtensions: ['ts', 'tsx', 'js'],
+}
 
-module.exports = nextConfig
+module.exports = {
+    skipMiddlewareUrlNormalize: true,
+    nextConfig,
+}
