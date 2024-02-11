401409e635ecbbe14aaffe77a9211803e8c3ea98
Jacek Michalski <jacmilski@gmail.com>
2024-02-11 08:13:37 +0100

    Add auth flow and layout

--- app/(application)/(app)/dashboard/page.tsx
+++ app/(application)/(app)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
   return (
-    <main className="mx-4  px-0 text-center text-zinc-900">Dashboard</main>
+    <main className="mx-4  px-0 text-center text-zinc-900">
+      <UserButton afterSignOutUrl="/" />
+      <p>Dashboard</p>
+    </main>
   );
 }
